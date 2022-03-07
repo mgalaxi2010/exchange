6c4591720b24012f33082e400e7eabefd306a34c
Mohammad Hasani <m.hasani@mhasani.ldc.local>
2022-03-07 10:01:31 +0330

    seeder edited

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,5 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(coinSeeder::class);
         $this->call(BrokerUserSeeder::class);
-         $this->call(transactionTypeSeeder::class);
     }
 }
